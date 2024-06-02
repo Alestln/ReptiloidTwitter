aab6a63c66218edc4c383b40b614393bc9ead63b
Alestln <itworkplace46@gmail.com>
2024-06-02 16:22:00 +0300

    [FEATURE] Added role Entity.cs

---
+++ Core/Domain/Roles/Models/Role.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.Domain.Roles.Models;
+
+public class Role
+{
+    public int Id { get; private set; }
+
+    public string Title { get; private set; }
+
+    private Role(string title)
+    {
+        Title = title;
+    }
+    
+    public static Role Create(string title)
+    {
+        return new Role(title: title);
+    }
+
+    public void Update(string title)
+    {
+        Title = title;
+    }
+}
