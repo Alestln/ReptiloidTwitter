d189d11e8a6d489fe8fc4f59e7553c4282da45f6
Alestln <itworkplace46@gmail.com>
2024-06-04 23:43:11 +0300

    [FEATURE] Lastname and Firstname is nullable properties in UserProfile entity

---
+++ Api/Common/ApiControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReptiloidTwitter.Common;
+
+[ApiController]
+public class ApiControllerBase : ControllerBase
+{
+    // Some extensions methods for controllers 
+}
--- Core/Domain/UserProfiles/Models/UserProfile.cs
+++ Core/Domain/UserProfiles/Models/UserProfile.cs
@@ -9,9 +9,9 @@ public class UserProfile : Entity
 {
     public Guid AccountId { get; private set; }
 
-    public string FirstName { get; private set; }
+    public string? FirstName { get; private set; }
     
-    public string LastName { get; private set; }
+    public string? LastName { get; private set; }
     
     public string? MiddleName { get; private set; }
 
