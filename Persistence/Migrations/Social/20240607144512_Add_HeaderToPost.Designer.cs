4399c31dfb126a7a15934c5d6fdf09a8c11a4ad1
Alestln <itworkplace46@gmail.com>
2024-06-07 17:46:45 +0300

    [FEATURE] Added migration

--- Persistence/Migrations/Social/20240607144512_Add_HeaderToPost.Designer.cs
+++ Persistence/Migrations/Social/20240607144512_Add_HeaderToPost.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.Contexts;
@@ -11,9 +12,11 @@ using Persistence.Contexts;
 namespace Persistence.Migrations.Social
 {
     [DbContext(typeof(SocialDbContext))]
-    partial class SocialDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240607144512_Add_HeaderToPost")]
+    partial class Add_HeaderToPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/Social/20240607144512_Add_HeaderToPost.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations.Social
+{
+    /// <inheritdoc />
+    public partial class Add_HeaderToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Header",
+                schema: "Social",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Header",
+                schema: "Social",
+                table: "Posts");
+        }
+    }
+}
